bc4bc6388fd6ec87eb0221bfee5f8e55be7bbbe5
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-05-03 15:19:48 -0700

    add scss partial's, add media query mixins, add scss to gulp folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,27 @@ const rename = require("gulp-rename");
 const browserSync = require("browser-sync");
 const eslint = require("gulp-eslint");
 const prettyError = require("gulp-prettyerror");
+// add just these 3 lines below
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cssnano = require("gulp-cssnano");
+
+// add a new task from the slides, gulp sass
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/style.scss")
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./build/css"))
+    .pipe(cssnano())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./build/css"));
+});
 
 gulp.task("lint", function() {
   return gulp
@@ -33,12 +54,13 @@ gulp.task("browser-sync", function(done) {
   }); // end of browserSync.init
 
   gulp
-    .watch(["index.html", "css/*.css", "build/js/*.js"])
+    .watch(["index.html", "build/css/*.css", "build/js/*.js"])
     .on("change", browserSync.reload);
 }); // browser-sync
 
 gulp.task("watch", function() {
   gulp.watch("js/*.js", gulp.series("scripts"));
+  gulp.watch("sass/**/*.scss", gulp.series("sass"));
 });
 
 // default gulp runs everything at once in this case
