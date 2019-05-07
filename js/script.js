1cb6fd07d653a2a193943e7b1f5be43b5b14b567
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-05-07 10:02:18 -0700

    add new filter function for multimedia and add slice funciton following

--- js/script.js
+++ js/script.js
@@ -2,32 +2,44 @@ $(function() {
   $("#select-form").on("change", function() {
     const selected = $(this).val();
     if (selected !== "") {
-      console.log(selected);
+      $(".sections").html("");
+      $("#loading").show();
       $.ajax({
         method: "GET",
         url:
           "https://api.nytimes.com/svc/topstories/v2/" +
           selected +
           ".json?api-key=CiPKzuEgMEr5iAez74KClBIAOmT4iTlJ"
-      }).done(function(data) {
-        $.each(data.results, function(index, value) {
-          const newLink = value.url;
-          const bg = value.multimedia[4].url;
-          const title = value.title;
-          const content = value.abstract;
-          console.log(bg);
-          bg;
-          $(".sections").append(
-            `<a href= "${newLink}" 
+      })
+        .done(function(data) {
+          const filteredData = data.results
+            .filter(function(article) {
+              return article.multimedia[4] !== undefined;
+            })
+            .slice(0, 12);
+          $.each(filteredData, function(index, value) {
+            const newLink = value.url;
+            const bg = value.multimedia[4].url;
+            const title = value.title;
+            const content = value.abstract;
+            $(".sections").append(
+              `<a href= "${newLink}" 
                 title="${title}" 
                 style="background-image: url(${bg});
                  background-repeat: no-repeat; 
                  background-size: cover; 
                  background-position: top center;">
                 <p class= "article-content">${content}</p></a>`
-          );
-        }); //end of .each
-      }); //end of .done
+            );
+            $("#loading").hide();
+          }); //end of .each
+        })
+        .fail(function() {
+          alert("Please try again");
+        })
+        .always(function() {
+          $("#loading").hide();
+        }); //end of .always and .fail
     } //end of if statement
   }); //end of on change function
 }); //end of Javascript
